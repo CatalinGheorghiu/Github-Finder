ab00b8e7830f493a3cb33788b44b7b41c97eb9c3
Ionut Catalin GHEORGHIU <ionutcatalingheorghiu@pop-os.localdomain>
2020-07-01 14:21:15 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Alert from "./components/layout/Alert";
+import User from "./components/users/User";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
+
+import "./App.css";
+
+// Component
+const App = () => {
+    return (
+        <GithubState>
+            <AlertState>
+                <Router>
+                    <div className="App">
+                        {/* Prop */}
+                        <Navbar />
+                        <div className="container">
+                            {/* Components */}
+                            <Alert />
+                            <Switch>
+                                {/* Route */}
+                                <Route exact path="/" component={Home} />
+                                {/* Route */}
+                                <Route exact path="/about" component={About} />
+                                {/* Route */}
+                                <Route
+                                    exact
+                                    path="/user/:login"
+                                    component={User}
+                                />
+                                <Route component={NotFound} />
+                            </Switch>
+                        </div>
+                    </div>
+                </Router>
+            </AlertState>
+        </GithubState>
+    );
+};
 
 export default App;
